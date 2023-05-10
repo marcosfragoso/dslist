4e8606c24318142bc3f3fe876af0bc07f5d4116b
Marcos <fragoso.ufcg@gmail.com>
2023-05-09 21:33:14 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,13 @@ import java.util.List;
 public class GameController { // Porta de entrada do back-end (O que disponibiliza/implementa a API)
     @Autowired
     private GameService gameService; // Injetando um Service no Controller
+
+    @GetMapping(value = "/{id}") // Verbo HTTP (endpoint para buscar)
+    public GameDTO findById(@PathVariable Long id) {  // Garantir que o id case com o id da requisicao
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
     @GetMapping // Verbo HTTP (endpoint para buscar)
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.findAll();
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dslist.services;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,13 @@ public class GameService { // O serviço devolve um DTO
 
     @Autowired
     private GameRepository gameRepository; // Injetando uma isntância do GameRepository
+    @Transactional // Garante que as operações do BD sejam realizadas
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get(); // Pega o Game pelo ID
+        GameDTO dto = new GameDTO(result); // Convertendo para DTO
+        return dto;
+    }
+    @Transactional
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll(); // Busca todos os games do BD e coloca numa lista
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); // Transforma uma lista de Games em uma de GameDTO
