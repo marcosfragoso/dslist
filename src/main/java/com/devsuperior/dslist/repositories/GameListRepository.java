f81909d2b97ffd4c1ebaf5bbb396baec0295b065
Marcos <fragoso.ufcg@gmail.com>
2023-05-12 18:05:26 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -3,8 +3,13 @@ package com.devsuperior.dslist.repositories;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 // Objeto responsavel por fazer consultas ao banco de dados
 public interface GameListRepository extends JpaRepository<GameList, Long> {
-
+    @Modifying  // Quando não é select, tem que colocar esse @
+    @Query(nativeQuery = true,
+            value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -5,6 +5,7 @@ import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
 import com.devsuperior.dslist.repositories.GameRepository;
 import jakarta.transaction.Transactional;
@@ -20,11 +21,27 @@ public class GameListService { // O serviço devolve um DTO
 
     @Autowired
     private GameListRepository gameListRepository; // Injetando uma isntância do GameRepository
+    @Autowired
+    private GameRepository gameRepository;
 
     @Transactional
     public List<GameListDTO> findAll() {
         List<GameList> result = gameListRepository.findAll(); // Busca todos os games do BD e coloca numa lista
         return result.stream().map(x -> new GameListDTO(x)).toList();
+
+    }
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
 
     }
 }
